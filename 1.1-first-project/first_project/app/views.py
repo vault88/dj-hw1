96747befd4e5c4aa4434a604629eda4c8b2ad039
zenday <ilinsky.dmitry@gmail.com>
2023-09-22 00:23:00 +0300

    homework-1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -8,8 +10,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,13 +25,15 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now().strftime("%H:%M:%S %d-%m-%Y")
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
+    work_directory = os.getcwd()
+    files = f'Список файлов в директории: {[x for x in os.listdir(work_directory)]}'
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    return HttpResponse(files)# raise NotImplemented
--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from app.views import home_view
+from app.views import home_view, time_view, workdir_view
 
 
 urlpatterns = [
@@ -26,4 +26,6 @@ urlpatterns = [
     # path('current_time/', time_view, name='time'),
     # path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+    path('time/', time_view, name='time'),
+    path('workdir/', workdir_view, name='workdir')
 ]
